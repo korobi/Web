aaa63a2a05a5bee67def9f3152f7b742dc41c4b3
lol768 <lol768@lol768.com>
2015-01-19 01:06:34 +0000

    Put laravel root in repo root, remove old files

--- old/README.md
+++
@@ -1,3 +0,0 @@
-Copyright 2014 - 2015 Kashike
-
-The code found in this respository is private, and is not to be shared with anyone.
--- old/assets/js/tail.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	setTimeout(function() {
-		location.reload();
-	},6000);
-});
-$(function() {
-	$(document).scrollTop($("#bottom").offset().top);
-});
