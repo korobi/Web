bb7372da9f41255dc47e6715d3aa9989c931981e
lol768 <lol768@lol768.com>
2015-01-22 22:59:44 +0000

    Fix up live command, add simple test JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var compass = require('gulp-compass'),
+var runSequence = require('run-sequence'),
+    compass = require('gulp-compass'),
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
@@ -7,7 +8,8 @@ var compass = require('gulp-compass'),
     path = require('path'),
     gulp = require('gulp'),
     git = require('gulp-git'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    shell = require('gulp-shell');
 
 
 gulp.task('deploy', function (cb) {
@@ -19,7 +21,7 @@ gulp.task('deploy', function (cb) {
 });
 
 //styles
-gulp.task('styles', ['deploy', 'scripts'],  function () {
+gulp.task('styles', ['scripts'], function () {
     return gulp.src(['resources/assets/sass/**/*.scss'])
         .pipe(compass({
             css: 'public/css',
@@ -32,7 +34,7 @@ gulp.task('styles', ['deploy', 'scripts'],  function () {
 });
 
 //scripts
-gulp.task('scripts', ['deploy'],  function () {
+gulp.task('scripts', function () {
     return gulp.src('resources/assets/js/**/*.js')
         .pipe(concat('app.js'))
         .pipe(gulp.dest('public/js'))
@@ -41,8 +43,10 @@ gulp.task('scripts', ['deploy'],  function () {
         .pipe(gulp.dest('public/js'));
 });
 
+gulp.task('serve', shell.task('php artisan serve'));
+
 //watch
-gulp.task('live', function () {
+gulp.task('watcher', function () {
     livereload.listen();
 
     //watch .scss files
@@ -50,9 +54,14 @@ gulp.task('live', function () {
 
     //watch .js files
     gulp.watch('resources/assets/js/**/*.js', ['scripts']);
-
-    gutil.log(gutil.colors.green('You may now change SASS and JS files.'));
+    gutil.log(gutil.colors.green('You may now change SASS and JS files!'));
 
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', function() {
+    runSequence("deploy", ["styles", "scripts"]);
+});
+
+gulp.task('live', function() {
+    runSequence(["watcher", "serve"]);
+});
