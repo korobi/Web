b391f0e1ff1c7ddef3cd062e33598c454c54d707
lol768 <lol768@lol768.com>
2015-01-22 22:31:48 +0000

    Continue work on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,55 @@
-var gulp = require('gulp');
-var git = require('gulp-git');
-var gutil = require('gulp-util');
+var compass = require('gulp-compass'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    path = require('path'),
+    gulp = require('gulp'),
+    git = require('gulp-git'),
+    gutil = require('gulp-util');
 
 
-gulp.task('deploy', function(){
-  git.pull('origin', 'laravel', {args: '--rebase'}, function (err) {
-    if (err) throw err;
-  });
+gulp.task('deploy', function () {
+    git.pull('origin', 'laravel', {args: '--rebase'}, function (err) {
+        if (err) throw err;
+    });
+
 });
+
+//styles
+gulp.task('styles', ['deploy', 'scripts'],  function () {
+    return gulp.src(['resources/assets/sass/**/*.scss'])
+        .pipe(compass({
+            css: 'public/css',
+            sass: 'resources/assets/sass/'
+        }))
+        .pipe(gulp.dest('public/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/css'));
+});
+
+//scripts
+gulp.task('scripts', ['deploy'],  function () {
+    return gulp.src('resources/assets/js/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('public/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'));
+});
+
+//watch
+gulp.task('live', function () {
+    livereload.listen();
+
+//watch .scss files
+    gulp.watch('resources/assets/sass/**/*.scss', ['styles']);
+
+//watch .js files
+    gulp.watch('resources/assets/js/**/*.js', ['scripts']);
+
+});
+
+gulp.task('default', ['styles']);
