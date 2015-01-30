1b3d8d1363355314e40a3cf633bce0b8167c2857
lol768 <lol768@lol768.com>
2015-01-30 22:08:23 +0000

    I'm stupid :P

--- config/app.php
+++ config/app.php
@@ -135,6 +135,7 @@ return [
         'Illuminate\Translation\TranslationServiceProvider',
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
         'Laravel\Socialite\SocialiteServiceProvider',
 
         /*
