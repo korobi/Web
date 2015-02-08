c840d8e1f123ec41760fab18078bc81324252ab2
Joshua Popoff <kashike@vq.lc>
2015-02-08 14:24:34 -0800

    Fix repository name, add helper funcion

--- src/Korobi/WebBundle/Repository/ChannelRepository.php
+++ src/Korobi/WebBundle/Repository/ChannelRepository.php
@@ -4,6 +4,17 @@ namespace Korobi\WebBundle\Repository;
 
 use Doctrine\ODM\MongoDB\DocumentRepository;
 
-class ChannelConfigRepository extends DocumentRepository {
+class ChannelRepository extends DocumentRepository {
 
+    /**
+     * @param $network
+     * @return mixed
+     */
+    public function findAllByNetwork($network) {
+        return $this->createQueryBuilder()
+            ->sort('name', 'ASC')
+            ->field('network')->equals($network)
+            ->getQuery()
+            ->execute();
+    }
 }
