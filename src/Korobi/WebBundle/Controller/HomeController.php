2e8a42f23f781b7452341b7a9f177adf7c4c23d9
lol768 <lol768@lol768.com>
2015-02-08 23:09:29 +0000

    Use injection on HomeController, finally got it working

--- src/Korobi/WebBundle/Controller/HomeController.php
+++ src/Korobi/WebBundle/Controller/HomeController.php
@@ -2,11 +2,29 @@
 
 namespace Korobi\WebBundle\Controller;
 
+use Korobi\WebBundle\Repository\ChannelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 
-class HomeController extends Controller {
+class HomeController {
+
+    /**
+     * @var ChannelRepository The channel repository.
+     */
+    private $channels;
+
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    public function __construct(EngineInterface $templating, ChannelRepository $channels) {
+        $this->channels = $channels;
+        $this->templating = $templating;
+    }
 
     public function homeAction() {
-        return $this->render('KorobiWebBundle::home.html.twig');
+        dump($this->channels->findAllByNetwork('solas')->toArray());
+        return $this->templating->renderResponse('KorobiWebBundle::home.html.twig');
     }
 }
