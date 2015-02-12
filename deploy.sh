9dc107e00414facb0f152d50245aec8a25513eba
lol768 <lol768@lol768.com>
2015-02-12 23:25:50 +0000

    Add host check to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if ["$(hostname)" == "iris.solas.io"]; then
+    ssh-agent bash -c 'ssh-add /home/kashike/.ssh/deploy_key; /data1/web/dev.korobi/io/deploy.sh'
+fi
 git pull
 sudo chmod -R 777 app/cache/dev
 php app/console assetic:dump
