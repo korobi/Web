41928d6fb029b392e415a89a464fc52018a34a49
Joshua Popoff <kashike@vq.lc>
2015-02-12 15:32:00 -0800

    @lol768 This time I'm the derp

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$(hostname)" == "iris.solas.io" ]; then
-    ssh-agent bash -c 'ssh-add /home/kashike/.ssh/deploy_key; /data1/web/dev.korobi/io/deploy.sh'
+    ssh-agent bash -c 'ssh-add /home/kashike/.ssh/deploy_key; /data1/web/dev.korobi.io/deploy.sh'
 fi
 git pull
 sudo chmod -R 777 app/cache/dev
