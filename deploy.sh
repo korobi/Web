f43edd094006b8586cfab9784bb848556ea776c8
root <root@solas.io>
2015-02-12 15:51:07 -0800

    Should work now... I think

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-sudo chmod -R 777 app/cache/dev
+chmod -R 777 app/cache/dev
 php app/console assetic:dump
 cd app/
 phpunit
--- deploy_init.sh
+++ deploy_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "$(hostname)" == "iris.solas.io" ]; then
-    ssh-agent bash -c 'ssh-add /home/kashike/.ssh/deploy_key; /data1/web/dev.korobi.io/deploy.sh'
+    ssh-agent bash -c 'ssh-add /data1/web/deploy_key; /data1/web/dev.korobi.io/deploy.sh'
 fi
