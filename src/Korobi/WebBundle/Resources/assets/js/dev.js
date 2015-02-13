8c274713cb395d25346d0da298f70189e6676dde
lol768 <lol768@lol768.com>
2015-02-13 21:53:36 +0000

    Move dev js to separate file

---
+++ src/Korobi/WebBundle/Resources/assets/js/dev.js
@@ -0,0 +1,33 @@
+$(function() {
+    var $debugToolbar = $(".sf-toolbar");
+    function getSymfonyPreference(name) {
+        if (!window.localStorage) {
+            return null;
+        }
+        return localStorage.getItem('sf2/profiler/' + name);
+    }
+
+    if ($debugToolbar.length != 0) {
+        var dispPref = getSymfonyPreference("toolbar/displayState");
+        if (dispPref == "block") {
+            $(".footer").addClass("footer--debug");
+            $("body").addClass("open-debug");
+        }
+    }
+
+    function toggleFooterDebugMode() {
+        var $debugToolbar = $(".sf-minitoolbar");
+        if ($debugToolbar.css("display") != "none") {
+            $(".footer").removeClass("footer--debug");
+            $("body").removeClass("open-debug");
+        } else {
+            $(".footer").addClass("footer--debug");
+            $("body").addClass("open-debug");
+        }
+
+    }
+
+    $("body").on("click", ".hide-button, a[title='Show Symfony toolbar']", function() {
+        toggleFooterDebugMode();
+    });
+});
