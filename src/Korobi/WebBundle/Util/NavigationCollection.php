251312d023fceeeac3f3da2e7956ab0d06433f3e
lol768 <lol768@lol768.com>
2015-02-13 19:54:43 +0000

    Begin work on navigation

---
+++ src/Korobi/WebBundle/Util/NavigationCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Korobi\WebBundle\Util;
+
+
+/**
+ * Holds many navigation items.
+ * @package Korobi\Util
+ */
+class NavigationCollection {
+
+    private $primary = [];
+    private $secondary = [];
+
+    public function __construct() {
+
+    }
+}
