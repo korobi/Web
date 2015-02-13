b962c473e86d23804d2ada7dd6e5e32dccb6ad90
lol768 <lol768@lol768.com>
2015-02-13 19:59:17 +0000

    Add a lot more debug messages to the deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+echo "Pulling..."
 git pull
+echo "chmoding..."
 chmod -R 777 app/cache/dev
+echo "Making asset files..."
 php app/console assetic:dump
+echo "Running tests..."
 cd app/
 phpunit
--- deploy_init.sh
+++ deploy_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+echo "Start deploy"
 if [ "$(hostname)" == "iris.solas.io" ]; then
+    echo "Detected iris.solas.io, adding deploy key"
     ssh-agent bash -c 'ssh-add /data1/web/deploy_key; /data1/web/dev.korobi.io/deploy.sh'
+else
+    echo "Just running deploy script"
+    ./deploy.sh
 fi
+echo "End deploy"
