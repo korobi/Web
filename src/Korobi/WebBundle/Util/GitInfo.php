7da5f540255633096827f13b5fe7824570cdce1b
lol768 <lol768@lol768.com>
2015-02-13 18:11:22 +0000

    Add back in the GitInfo utility class

---
+++ src/Korobi/WebBundle/Util/GitInfo.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Korobi\WebBundle\Util;
+
+/**
+ * Class to handle getting info about git branches, commits etc.
+ * @package Korobi\WebBundle\Util
+ */
+class GitInfoUtility {
+
+    protected $filesystem;
+    protected $branch;
+    protected $hash;
+
+    /**
+     * Initialize the class.
+     */
+    public function __construct() {
+        $root = __DIR__ . "/../../../../"; // bit of a hack
+        $ref = (new \SplFileObject($root . '.git/HEAD'))->getCurrentLine();
+        $ref = trim(explode(' ', $ref)[1]);
+
+        $this->branch = trim(array_reverse(explode('/', $ref))[0]);
+        $root = $root . '.git/' . $ref;
+        $this->hash = $this->filesystem->get($root);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBranch() {
+        return $this->branch;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHash() {
+        return $this->hash;
+    }
+
+    /**
+     * @param int $length Length
+     * @return string
+     */
+    public function getShortHash($length = 8) {
+        return substr($this->hash, 0, $length);
+    }
+}
