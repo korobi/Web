677822d4edd28db115a4df95c576d4bc7a3b22d2
lol768 <lol768@lol768.com>
2015-02-13 19:49:24 +0000

    Add NavigationItem class to represent different items of nav
    TODO: Share to all templates

---
+++ src/Korobi/WebBundle/Util/NavigationItem.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Korobi\WebBundle\Util;
+
+
+/**
+ * POPO for NavigationItems
+ * @package Korobi\Util
+ */
+class NavigationItem {
+
+    private $title;
+    private $url;
+    private $requiresAuth;
+    private $requiresAdmin;
+
+    public function __construct($requiresAdmin, $requiresAuth, $title, $url) {
+        $this->requiresAdmin = $requiresAdmin;
+        $this->requiresAuth = $requiresAuth;
+        $this->title = $title;
+        $this->url = $url;
+    }
+
+    /**
+     * @return boolean Whether or not the visibility of this item requires authentication.
+     */
+    public function getRequiresAdmin() {
+        return $this->requiresAdmin;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function getRequiresAuth() {
+        return $this->requiresAuth;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl() {
+        return $this->url;
+    }
+}
