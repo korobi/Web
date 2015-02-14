87aba136daf1a9c4a7bdf59fa6b7fb0675f418fc
lol768 <lol768@lol768.com>
2015-02-14 21:51:31 +0000

    Use merged configs

--- src/Korobi/WebBundle/DependencyInjection/KorobiWebExtension.php
+++ src/Korobi/WebBundle/DependencyInjection/KorobiWebExtension.php
@@ -11,7 +11,7 @@ class KorobiWebExtension extends Extension {
 
     public function load(array $config, ContainerBuilder $container) {
         $configuration = new Configuration();
-        $this->processConfiguration($configuration, $config);
+        $config = $this->processConfiguration($configuration, $config);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
