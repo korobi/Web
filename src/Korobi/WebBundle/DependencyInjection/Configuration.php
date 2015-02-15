67d25155259bff86cf447a3d75550cbd19db8d39
Joshua Popoff <kashike@vq.lc>
2015-02-14 16:26:00 -0800

    Format DI Configuration

--- src/Korobi/WebBundle/DependencyInjection/Configuration.php
+++ src/Korobi/WebBundle/DependencyInjection/Configuration.php
@@ -10,10 +10,18 @@ class Configuration implements ConfigurationInterface {
     public function getConfigTreeBuilder() {
         $treeBuilder = new TreeBuilder();
         $root = $treeBuilder->root('korobi_web');
-        $root->children()->arrayNode("navigation")->children()->arrayNode("items")->prototype("array")
-        ->children()->scalarNode("title")->end()->scalarNode("route")->end()->scalarNode("type")->defaultValue("primary")
-        ->end()->booleanNode("requires_auth")->defaultValue(false)->end()->booleanNode("requires_admin")->defaultValue(false)->end()
-        ->booleanNode("external")->defaultValue(false)->end()->end()->end();
+        $root->children()
+            ->arrayNode('navigation')->children()
+                ->arrayNode('items')->prototype('array')
+                    ->children()
+                        ->scalarNode('title')->end()
+                        ->scalarNode('route')->end()
+                        ->scalarNode('type')->defaultValue('primary')->end()
+                        ->booleanNode('requires_auth')->defaultValue(false)->end()
+                        ->booleanNode('requires_admin')->defaultValue(false)->end()
+                        ->booleanNode('external')->defaultValue(false)->end()
+                    ->end()
+                ->end();
         return $treeBuilder;
     }
 }
