24d5f2b07f88f216a8d3e85a719cca842092d13d
lol768 <lol768@lol768.com>
2015-02-15 00:26:20 +0000

    Handle light themes

---
+++ src/Korobi/WebBundle/Util/ThemeService.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Korobi\WebBundle\Util;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+class ThemeService {
+
+    /**
+     * @var Session
+     */
+    private $session;
+
+    /**
+     * @param Session $session
+     */
+    public function __construct(Session $session) {
+        $this->session = $session;
+    }
+
+    public function isLight() {
+        return $this->session->has("light-theme");
+    }
+}
