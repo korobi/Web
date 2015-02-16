a71efb2ab28faeef99616e2daed4dc0489306acc
lol768 <lol768@lol768.com>
2015-02-16 00:07:48 +0000

    Add revision document and repository

---
+++ src/Korobi/WebBundle/Document/Revision.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace Korobi\WebBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document(collection="revisions",repositoryClass="Korobi\WebBundle\Repository\RevisionRepository")
+ */
+class Revision {
+
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\String
+     */
+    private $old_commit;
+
+    /**
+     * @MongoDB\String
+     */
+    private $new_commit;
+
+    /**
+     * @MongoDB\Date
+     */
+    private $date;
+
+    /**
+     * @MongoDB\Boolean
+     */
+    private $deploy_successful;
+
+    /**
+     * @MongoDB\String
+     */
+    private $deploy_output;
+
+    /**
+     * @MongoDB\Boolean
+     */
+    private $tests_passed;
+
+    /**
+     * @MongoDB\String
+     */
+    private $tests_output;
+
+    /**
+     * @MongoDB\Boolean
+     */
+    private $manual;
+
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Set oldCommit
+     *
+     * @param string $oldCommit
+     * @return self
+     */
+    public function setOldCommit($oldCommit) {
+        $this->old_commit = $oldCommit;
+        return $this;
+    }
+
+    /**
+     * Get oldCommit
+     *
+     * @return string $oldCommit
+     */
+    public function getOldCommit() {
+        return $this->old_commit;
+    }
+
+    /**
+     * Set newCommit
+     *
+     * @param string $newCommit
+     * @return self
+     */
+    public function setNewCommit($newCommit) {
+        $this->new_commit = $newCommit;
+        return $this;
+    }
+
+    /**
+     * Get newCommit
+     *
+     * @return string $newCommit
+     */
+    public function getNewCommit() {
+        return $this->new_commit;
+    }
+
+    /**
+     * Set date
+     *
+     * @param date $date
+     * @return self
+     */
+    public function setDate($date) {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return date $date
+     */
+    public function getDate() {
+        return $this->date;
+    }
+
+    /**
+     * Set deploySuccessful
+     *
+     * @param boolean $deploySuccessful
+     * @return self
+     */
+    public function setDeploySuccessful($deploySuccessful) {
+        $this->deploy_successful = $deploySuccessful;
+        return $this;
+    }
+
+    /**
+     * Get deploySuccessful
+     *
+     * @return boolean $deploySuccessful
+     */
+    public function getDeploySuccessful() {
+        return $this->deploy_successful;
+    }
+
+    /**
+     * Set deployOutput
+     *
+     * @param string $deployOutput
+     * @return self
+     */
+    public function setDeployOutput($deployOutput) {
+        $this->deploy_output = $deployOutput;
+        return $this;
+    }
+
+    /**
+     * Get deployOutput
+     *
+     * @return string $deployOutput
+     */
+    public function getDeployOutput() {
+        return $this->deploy_output;
+    }
+
+    /**
+     * Set testsPassed
+     *
+     * @param boolean $testsPassed
+     * @return self
+     */
+    public function setTestsPassed($testsPassed) {
+        $this->tests_passed = $testsPassed;
+        return $this;
+    }
+
+    /**
+     * Get testsPassed
+     *
+     * @return boolean $testsPassed
+     */
+    public function getTestsPassed() {
+        return $this->tests_passed;
+    }
+
+    /**
+     * Set testsOutput
+     *
+     * @param string $testsOutput
+     * @return self
+     */
+    public function setTestsOutput($testsOutput) {
+        $this->tests_output = $testsOutput;
+        return $this;
+    }
+
+    /**
+     * Get testsOutput
+     *
+     * @return string $testsOutput
+     */
+    public function getTestsOutput() {
+        return $this->tests_output;
+    }
+
+    /**
+     * Set whether deploy was done manually
+     *
+     * @param boolean $manual
+     * @return self
+     */
+    public function setManual($manual) {
+        $this->manual = $manual;
+        return $this;
+    }
+
+    /**
+     * Get whether deploy was done manually
+     *
+     * @return boolean $manual
+     */
+    public function getManual() {
+        return $this->manual;
+    }
+}
---
+++ src/Korobi/WebBundle/Repository/RevisionRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Korobi\WebBundle\Repository;
+
+use Doctrine\ODM\MongoDB\DocumentRepository;
+
+class RevisionRepository extends DocumentRepository {
+
+}
