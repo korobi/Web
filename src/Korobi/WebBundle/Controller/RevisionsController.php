b92c7b10aee0795d7c22dbbcf9651c23f7bd49fa
kashike <kashike@vq.lc>
2015-02-17 10:40:08 -0800

    Revisions work, kinda

--- src/Korobi/WebBundle/Controller/RevisionsController.php
+++ src/Korobi/WebBundle/Controller/RevisionsController.php
@@ -2,9 +2,26 @@
 
 namespace Korobi\WebBundle\Controller;
 
+use Github\Client;
+
 class RevisionsController extends BaseController {
 
-    public function listAction() {
-        return $this->render('KorobiWebBundle::revisions.html.twig');
+    private static $repositories = ['Web', 'Freya', 'Akio'];
+
+    public function listAction($repository) {
+        if (!in_array($repository, self::$repositories)) {
+            return $this->redirectToRoute('revisions');
+        }
+
+        $client = new Client();
+        $client->authenticate($this->container->getParameter('github_oauth_token'), Client::AUTH_HTTP_TOKEN);
+
+        return $this->render('KorobiWebBundle::revisions.html.twig', [
+            'repository' => $repository,
+            'revisions' => $client
+                ->api('repo')
+                ->commits()
+                ->all('korobi', $repository, ['sha' => 'master'])
+        ]);
     }
 }
--- src/Korobi/WebBundle/Extension/ExtensionTwigFilters.php
+++ src/Korobi/WebBundle/Extension/ExtensionTwigFilters.php
@@ -12,11 +12,20 @@ class ExtensionTwigFilters extends \Twig_Extension {
 
     public function getFilters() {
         return [
-            new \Twig_SimpleFilter('ircformat', [$this, 'ircFormat'])
+            new \Twig_SimpleFilter('ircformat', [$this, 'ircFormat']),
+            new \Twig_SimpleFilter('commitauthorname', [$this, 'replaceAuthorName'])
         ];
     }
 
     public function ircFormat($string) {
         return IRCTextParser::parse($string);
+    }
+
+    public function replaceAuthorName($name) {
+        if ($name === 'Joshua Popoff') {
+            return 'kashike';
+        }
+
+        return $name;
     }
 }
