2671ec67634afbeeb59cdaa303ed0f58dc2feddc
kashike <kashike@vq.lc>
2015-02-19 00:07:55 -0800

    Implement line highlight selection/deselection

--- src/Korobi/WebBundle/Resources/assets/js/prod/application.js
+++ src/Korobi/WebBundle/Resources/assets/js/prod/application.js
@@ -1,50 +1,129 @@
 $(function() {
-    function highlightLine(lineNum) {
-        var selector = ".logs--line[data-line-num='" + lineNum + "']";
-        $(selector).addClass("highlighted");
-    }
+    var activeLines = [],
+        /**
+         * @param line
+         */
+        addLine = function(line) {
+            // we don't want zero
+            if (line === 0) {
+                return;
+            }
 
-    function hashChange() {
-        $(".logs--line").removeClass("highlighted");
-        var hash = window.location.hash;
-        var remainingPart = hash.substr(0, 2);
-        if (remainingPart == "#L") {
-            if ($("div.logs").length == 0) {
-                // meow
-            } else {
-                var remainingParts = hash.substr(2).split(",");
-                for (var i = 0; i < remainingParts.length; i++) {
-                    var part = remainingParts[i];
+            // only add if not already in array
+            if (activeLines.indexOf(line) == -1) {
+                activeLines.push(line);
+            }
 
-                    if (part.indexOf("-") !== -1) {
-                        var re = /([0-9]+)-([0-9]+)/;
-                        var match = re.exec(part);
-                        if (match.length !== 3) {
-                        } else {
-                            var imin = Number(match[1]);
-                            var imax = Number(match[2]);
-                            for (var j = imin; j <= imax; j++) {
-                                highlightLine(j);
+            // add class
+            $(".logs--line[data-line-num='" + line + "']").addClass('highlighted');
+        },
+        /**
+         * @param line
+         */
+        removeLine = function(line) {
+            // remove line from array
+            var index;
+            if ((index = activeLines.indexOf(line)) > -1) {
+                activeLines.splice(index, 1);
+            }
+
+            // remove class
+            $(".logs--line[data-line-num='" + line + "']").removeClass('highlighted');
+        },
+        /**
+         * @returns {string}
+         */
+        createHash = function() {
+            var result = 'L'; // hash (#) is automatically added
+            var first = true;
+
+            activeLines.forEach(function(entry) {
+                if (!first) {
+                    result += ',';
+                }
+
+                first = false;
+                result += entry;
+            });
+
+            return result;
+        },
+        hashChange = function() {
+            $('.logs--line').removeClass('highlighted');
+
+            var hash = window.location.hash;
+
+            var remainingPart = hash.substr(0, 2);
+            if (remainingPart == "#L") {
+                if ($('div.logs').length !== 0) {
+                    var remainingParts = hash.substr(2).split(',');
+                    for (var i = 0; i < remainingParts.length; i++) {
+                        var part = remainingParts[i];
+
+                        if (part.indexOf('-') !== -1) {
+                            var re = /([0-9]+)-([0-9]+)/;
+                            var match = re.exec(part);
+                            if (match.length === 3) {
+                                var imin = Number(match[1]);
+                                var imax = Number(match[2]);
+                                for (var j = imin; j <= imax; j++) {
+                                    addLine(j);
+                                }
                             }
-                        }
-                    } else {
-                        var ival = Number(part);
-                        if (isNaN(ival)) {
-                            // meow
                         } else {
-                            highlightLine(ival);
+                            var lineNum = Number(part);
+                            if (!isNaN(lineNum)) {
+                                addLine(lineNum);
+                            }
                         }
                     }
                 }
             }
-        }
-    }
+        },
+        jumpToFirstLine = function() {
+            if (activeLines.length > 0) {
+                var shift = Math.min.apply(Math, activeLines);
+                var elem = $(".logs--line[data-line-num='" + shift + "']");
+                $(window).scrollTop(elem.offset().top - 0.33 * $(window).height());
+            }
+        };
 
+    // listen for hash changes
     $(window).on('hashchange', function() {
         hashChange();
     });
 
+    // allow adding and removing lines using the icon beside the log line
+    $(document).on('mousedown', '.js-hl .fa', function(event) {
+        event.preventDefault();
+
+        // grab parent (.fa is a child)
+        var line = $(this).parent().data('line-num');
+
+        // if shift is being held, add another line
+        if (event.shiftKey) {
+            // if we're shifting and the line exists, remove it
+            if (activeLines.indexOf(line) != -1) {
+                removeLine(line);
+            } else {
+                addLine(line);
+            }
+        } else {
+            // shift is not being held, remove all and add new
+            activeLines.forEach(function(entry) {
+                removeLine(entry);
+            });
+
+            addLine(line);
+        }
+
+        // set the new hash
+        window.location.hash = createHash();
+    });
+
+    // delay load
     setTimeout(hashChange, 500);
+    setTimeout(jumpToFirstLine, 600);
 });
 
 $(function() {
