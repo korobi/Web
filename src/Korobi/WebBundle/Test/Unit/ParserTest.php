a84ede3f3c3e48ac4243fa7588db3ce07e0eea7c
lol768 <lol768@lol768.com>
2015-02-20 21:57:51 +0000

    Add a simple parser test function, still not sure how I feel about having this function return HTML

--- src/Korobi/WebBundle/Test/Unit/ParserTest.php
+++ src/Korobi/WebBundle/Test/Unit/ParserTest.php
@@ -2,12 +2,34 @@
 
 namespace Korobi\WebBundle\Test\Unit;
 
+use Korobi\WebBundle\Document\Chat;
+use Korobi\WebBundle\Parser\LogParser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Class ParserTest
+ * @see LogParser
+ * @package Korobi\WebBundle\Test\Unit
+ */
 class ParserTest extends WebTestCase {
 
     public function testShutUpPhpUnit() {
         $this->assertTrue(true);
     }
+
+    public function testParseJoin() {
+        /** @see LogParser::parseJoin */
+        $chat = $this->getMockBuilder('Korobi\WebBundle\Document\Chat')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $chat->expects($this->any())->method("getDate")->will($this->returnValue(new \DateTime("@0")));
+        $chat->expects($this->any())->method("getActorName")->will($this->returnValue("TestUser"));
+        $chat->expects($this->any())->method("getActorPrefix")->will($this->returnValue("OPERATOR"));
+
+        $this->assertEquals('[00:00:00] <span class="irc--14-99">** <span class="irc--09-99">@</span>TestUser joined the channel</span>', LogParser::parseJoin($chat));
+    }
+
+
 
 }
