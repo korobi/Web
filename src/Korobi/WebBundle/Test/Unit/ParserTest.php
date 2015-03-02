2fc64d8e338aa1b21454fffde5917c2212c2869d
lol768 <lol768@lol768.com>
2015-03-02 00:24:47 +0000

    Add failing test for reversed/inversed colours

--- src/Korobi/WebBundle/Test/Unit/ParserTest.php
+++ src/Korobi/WebBundle/Test/Unit/ParserTest.php
@@ -44,5 +44,19 @@ class ParserTest extends WebTestCase {
     }
 
 
+    public function testParseMessageWithReverseColour() {
+        /** @see LogParser::parseMessage */
+        $chat = $this->getMockBuilder('Korobi\WebBundle\Document\Chat')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $chat->expects($this->any())->method("getDate")->will($this->returnValue(new \DateTime("@0")));
+        $chat->expects($this->any())->method("getActorName")->will($this->returnValue("TestUser"));
+        $chat->expects($this->any())->method("getActorPrefix")->will($this->returnValue("OPERATOR"));
+        $chat->expects($this->any())->method("getMessage")->will($this->returnValue("\x0307,04\x16Hello!"));
+        $this->assertEquals('[00:00:00] &lt;<span class="irc--09-99">@</span><span class="irc--06-99">TestUser</span>&gt; <span class="irc--04-07">Hello!</span>', LogParser::parseMessage($chat));
+    }
+
+
 
 }
