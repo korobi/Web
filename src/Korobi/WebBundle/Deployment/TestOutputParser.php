71c0f95681db8592549cae17b20a1e1196d6fbb3
lol768 <lol768@lol768.com>
2015-03-04 00:44:18 +0000

    It's okay though, because the regex is backed with unit tests!

--- src/Korobi/WebBundle/Deployment/TestOutputParser.php
+++ src/Korobi/WebBundle/Deployment/TestOutputParser.php
@@ -7,17 +7,26 @@ namespace Korobi\WebBundle\Deployment;
 class TestOutputParser {
 
     public static function parseLine($line) {
-        $data = ["passed" => 0, "failed" => 0, "assertions" => 0];
+        $data = ["incomplete" => 0, "passed" => 0, "failures" => 0, "assertions" => 0, "status" => "Pass"];
         $regex = '/(?J)(?|Tests: (?P<tests>[0-9]+), Assertions: (?P<assertions>[0-9]+), Failures: (?P<failures>[0-9]+)(?:, Incomplete: (?P<incomplete>[0-9]+))?)|(?|OK \((?P<tests>[0-9]+) tests, (?P<assertions>[0-9]+) assertions\))|(?|Tests: (?P<tests>[0-9]+), Assertions: (?P<assertions>[0-9]+), Incomplete: (?P<incomplete>[0-9]+)\.)/';
         $matches = [];
         preg_match($regex, $line, $matches);
 
-        if (count($matches) === 4) {
 
-        } else {
-
-        }
         $data = array_merge($data, $matches);
+        foreach ($data as $key => $arr) {
+            if (is_int($key)) {
+                unset($data[$key]);
+            }
+        }
+        if ($data['incomplete'] > 0) {
+            $data['status'] = "Tentative pass";
+        }
+
+        if ($data['failures'] > 0) {
+            $data['status'] = "Fail";
+        }
+
         return $data;
     }
 
--- src/Korobi/WebBundle/Test/Unit/TestOutputParserTest.php
+++ src/Korobi/WebBundle/Test/Unit/TestOutputParserTest.php
@@ -12,12 +12,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class TestOutputParserTest extends WebTestCase {
 
-    public function testParseLine() {
+    public function testParseLineWithFailures() {
         $sut = new TestOutputParser();
         $data = $sut->parseLine("Tests: 23, Assertions: 29, Failures: 1.");
         $this->assertEquals(23, $data['tests']);
         $this->assertEquals(29, $data['assertions']);
         $this->assertEquals(1, $data['failures']);
+    }
+
+    public function testParseLineWithIncomplete() {
+        $sut = new TestOutputParser();
+        $data = $sut->parseLine("Tests: 23, Assertions: 28, Incomplete: 1.");
+        $this->assertEquals(23, $data['tests']);
+        $this->assertEquals(28, $data['assertions']);
+        $this->assertEquals(1, $data['incomplete']);
+        $this->assertEquals("Tentative pass", $data['status']);
+    }
+
+    public function testParseLineWithPass() {
+        $sut = new TestOutputParser();
+        $data = $sut->parseLine("OK (24 tests, 35 assertions)");
+        $this->assertEquals(24, $data['tests']);
+        $this->assertEquals(35, $data['assertions']);
+        $this->assertEquals("Pass", $data['status']);
     }
 
 }
