6ec21c620557894cafcb185943a8b8ba50bc8fcb
lol768 <lol768@lol768.com>
2015-03-05 23:05:25 +0000

    Cache clear after

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,15 @@ git pull
 echo "** Assigning directory permissions..."
 chmod -R 777 app/cache/dev
 
-echo "** Dumping asset files..."
-php app/console assetic:dump
-
-echo "** Clearing cache..."
-php app/console cache:clear
-
 echo "** Checking for dependency changes"
 git --no-pager log -1 HEAD --name-only | grep "composer.lock"
 if [ $? -ne 1 ]; then
     echo "** Found composer lock change, performing install"
     composer install
 fi
+
+echo "** Dumping asset files..."
+php app/console assetic:dump
+
+echo "** Clearing cache..."
+php app/console cache:clear
