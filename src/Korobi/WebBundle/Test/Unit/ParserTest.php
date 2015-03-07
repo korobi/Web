d7142f23d8da7169427d5c1a7203478629b746ca
lol768 <lol768@lol768.com>
2015-03-07 12:31:30 +0000

    Add test case for single colour reset

--- src/Korobi/WebBundle/Test/Unit/ParserTest.php
+++ src/Korobi/WebBundle/Test/Unit/ParserTest.php
@@ -58,6 +58,21 @@ class ParserTest extends WebTestCase {
         $this->assertEquals('<span class="irc--07-04"><span class="irc--04-07">Hello!</span></span>', LogParser::parseMessage($chat));
     }
 
+    public function testParseMessageWithSingleColourAsReset() {
+        /** @see LogParser::parseMessage */
+        $chat = $this->getMockBuilder('Korobi\WebBundle\Document\Chat')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // {3}13Red_Sky{3} plays {3}0,04 Red 7 {3}  {3}to {3}06Kashike
+
+        $chat->expects($this->any())->method("getDate")->will($this->returnValue(new \DateTime("@0")));
+        $chat->expects($this->any())->method("getActorName")->will($this->returnValue("TestUser"));
+        $chat->expects($this->any())->method("getActorPrefix")->will($this->returnValue("OPERATOR"));
+        $chat->expects($this->any())->method("getMessage")->will($this->returnValue("\x0313Red_Sky\x03 plays \x030,04 Red 7 \x03 \x03 to \x0306Kashike"));
+        $this->assertEquals('<span class="irc--13-99">Red_Sky</span> plays <span class="irc--00-04"> Red 7 </span>  to <span class="irc--06-04">Kashike</span>', LogParser::parseMessage($chat));
+    }
+
 
 
 }
