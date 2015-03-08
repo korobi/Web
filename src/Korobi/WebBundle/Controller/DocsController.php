867ff25d1680a2e0b50d217a95a5b0418976f5d8
lol768 <lol768@lol768.com>
2015-03-08 16:40:33 +0000

    Add docs controller for viewing docs

---
+++ src/Korobi/WebBundle/Controller/DocsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Korobi\WebBundle\Controller;
+
+use Symfony\Component\Intl\Exception\NotImplementedException;
+
+class DocsController extends BaseController {
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderAction($file) {
+        throw new NotImplementedException("Not yet implemented");
+    }
+}
