82b838516f11512776f4d7db5f2a827398089e78
lol768 <lol768@lol768.com>
2015-03-08 16:43:15 +0000

    I shouldn't use that exception

--- src/Korobi/WebBundle/Controller/DocsController.php
+++ src/Korobi/WebBundle/Controller/DocsController.php
@@ -2,13 +2,11 @@
 
 namespace Korobi\WebBundle\Controller;
 
-use Symfony\Component\Intl\Exception\NotImplementedException;
+
+use Korobi\WebBundle\Exception\NotImplementedException;
 
 class DocsController extends BaseController {
 
-    /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
     public function renderAction($file) {
         throw new NotImplementedException("Not yet implemented");
     }
---
+++ src/Korobi/WebBundle/Exception/NotImplementedException.php
@@ -0,0 +1,7 @@
+<?php
+
+
+namespace Korobi\WebBundle\Exception;
+
+
+class NotImplementedException extends \Exception {}
