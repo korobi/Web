01ab81a3ee8aea2294155b7cafb729439ce4b3e2
lol768 <lol768@lol768.com>
2015-03-08 14:58:52 +0000

    Everyone loves JSON

--- src/Korobi/WebBundle/Deployment/Processor/RunTests.php
+++ src/Korobi/WebBundle/Deployment/Processor/RunTests.php
@@ -33,7 +33,7 @@ class RunTests extends BaseProcessor implements DeploymentProcessorInterface {
         }
 
         $this->akio->sendMessage($message);
-
+        $this->akio->sendMessage(json_encode($parsed));
         $deploymentInfo->getRevision()->setTestsOutput(implode("\n", $execOutput));
         $deploymentInfo->getRevision()->setTestsPassed($parsed['failures'] === 0);
         $deploymentInfo->getRevision()->setTestsInfo($parsed);
