a86f96f1286292e7a8c419accfa1a38b4643621e
lol768 <lol768@lol768.com>
2015-03-08 16:56:41 +0000

    First try at using the markdown parser

--- src/Korobi/WebBundle/Controller/DocsController.php
+++ src/Korobi/WebBundle/Controller/DocsController.php
@@ -4,10 +4,24 @@ namespace Korobi\WebBundle\Controller;
 
 
 use Korobi\WebBundle\Exception\NotImplementedException;
+use Michelf\Markdown;
 
 class DocsController extends BaseController {
 
     public function renderAction($file) {
-        throw new NotImplementedException("Not yet implemented");
+        if (!preg_match("/^[A-Za-z0-9_]+$/", $file)) {
+            throw $this->createAccessDeniedException("Invalid doc");
+        }
+
+        $fn = $this->get('kernel')->getRootDir() . "/docs/" . $file . ".md";
+        if (!file_exists($fn)) {
+            throw $this->createAccessDeniedException("Doc does not exist");
+        }
+
+        $parser = new Markdown;
+        $parser->no_entities = true;
+        $parser->no_markup = true;
+
+        return $parser->transform(file_get_contents($fn));
     }
 }
