4c5741aa9c7854235bafca39482d87fc0fb48fba
kashike <kashike@vq.lc>
2015-03-10 13:25:36 -0700

    Let's try that again

--- src/Korobi/WebBundle/Repository/ChannelRepository.php
+++ src/Korobi/WebBundle/Repository/ChannelRepository.php
@@ -27,7 +27,7 @@ class ChannelRepository extends DocumentRepository {
         return $this->createQueryBuilder()
             ->sort('channel', 'ASC')
             ->field('network')->equals($network)
-            ->field('channel')->equals(new \MongoRegex('/^' . $channel . '/'))
+            ->field('channel')->equals(new \MongoRegex('/^' . $channel . '/i'))
             ->getQuery()
             ->execute();
     }
