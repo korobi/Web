9ecff40c1098f749097fe9cc313aae61ff7384aa
kashike <kashike@vq.lc>
2015-03-18 18:55:53 -0700

    Cachebusting

--- src/Korobi/WebBundle/Extension/ExtensionTwigGlobals.php
+++ src/Korobi/WebBundle/Extension/ExtensionTwigGlobals.php
@@ -2,7 +2,21 @@
 
 namespace Korobi\WebBundle\Extension;
 
+use Korobi\WebBundle\Util\GitInfo;
+
 class ExtensionTwigGlobals extends \Twig_Extension {
+
+    /**
+     * @var GitInfo
+     */
+    private $gitInfo;
+
+    /**
+     * @param GitInfo $gitInfo
+     */
+    public function __construct(GitInfo $gitInfo) {
+        $this->gitInfo = $gitInfo;
+    }
 
     public function getName() {
         return 'korobi_extension_twig_globals';
@@ -11,6 +25,7 @@ class ExtensionTwigGlobals extends \Twig_Extension {
     public function getGlobals() {
         return [
             'app_name' => 'Korobi',
+            'git_hash' => $this->gitInfo->getShortHash()
         ];
     }
 }
