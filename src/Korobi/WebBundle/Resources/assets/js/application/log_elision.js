69e8a9ae0f2fa31ba842fa9525376f07eb07334a
bendem <online@bendem.be>
2015-03-25 23:52:59 +0100

    Add tooltips to elided nicknames

---
+++ src/Korobi/WebBundle/Resources/assets/js/application/log_elision.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.line .nick').each(function(i, e) {
+        var width = e.scrollWidth - e.offsetWidth;
+        if(width > 0) {
+            $(e).attr('title', e.innerText).attr('rel', 'tooltip');
+        }
+    });
+});
