c784fc1703d6dd67bb682134a418ab3428776166
bendem <online@bendem.be>
2015-03-26 00:20:58 +0100

    Underline elided nicknames

--- src/Korobi/WebBundle/Resources/assets/js/application/log_elision.js
+++ src/Korobi/WebBundle/Resources/assets/js/application/log_elision.js
@@ -2,7 +2,15 @@ $(function() {
     $('.line .nick').each(function(i, e) {
         var width = e.scrollWidth - e.offsetWidth;
         if(width > 0) {
-            $(e).attr('title', e.innerText).attr('rel', 'tooltip');
+            $(e)
+                .attr('title', e.innerText)
+                .attr('rel', 'tooltip')
+                .css({
+                    'text-decoration': 'underline',
+                    'text-decoration-style': 'dotted',
+                    'text-decoration-color': '#444'
+                })
+            ;
         }
     });
 });
