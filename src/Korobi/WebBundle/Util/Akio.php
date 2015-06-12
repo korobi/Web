1a163d8a28ed2dd3848a3795f6f271d9fed7d524
kashike <kashike@vq.lc>
2015-06-12 10:34:58 -0700

    Make Akio configurable

--- src/Korobi/WebBundle/Util/Akio.php
+++ src/Korobi/WebBundle/Util/Akio.php
@@ -12,16 +12,19 @@ use GuzzleHttp\Client as GuzzleClient;
 class Akio {
 
     protected $guzzle;
+    private $enabled;
     private $url;
     private $key;
 
     /**
      * Initialize the class.
+     * @param $enabled If Akio is enabled
      * @param string $url The instance url
      * @param string $key The auth key
      */
-    public function __construct($url, $key) {
+    public function __construct($enabled, $url, $key) {
         $this->guzzle = new GuzzleClient();
+        $this->enabled = $enabled;
         $this->url = $url;
         $this->key = $key;
     }
@@ -37,10 +40,12 @@ class Akio {
      * @param AkioMessageBuilder $message
      */
     public function sendMessage(AkioMessageBuilder $message) {
-        $text = $message->getRawText();
-        $this->guzzle->get($this->url, [
-            'query' => ['message' => $text],
-            'headers' => ['X-Akio-Korobi' => $this->key]
-        ]);
+        if ($this->enabled) {
+            $text = $message->getRawText();
+            $this->guzzle->get($this->url, [
+                'query' => ['message' => $text],
+                'headers' => ['X-Akio-Korobi' => $this->key]
+            ]);
+        }
     }
 }
