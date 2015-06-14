ea4c9935f04f1091bbfb3c7de606927fbb3e5149
bendem <online@bendem.be>
2015-06-14 21:57:09 +0200

    Save user prefs in localStorage

--- src/Korobi/WebBundle/Resources/assets/js/application/log_controls.js
+++ src/Korobi/WebBundle/Resources/assets/js/application/log_controls.js
@@ -1,13 +1,44 @@
 $(function() {
     var $controls = $('.controls');
-    var statusEvents = ['join', 'quit', 'part'];
-    var hiddenEvents = [];
-
     if(!$controls.length) {
         return;
     }
 
-    $controls.find('#toggle-join-part').change(function() {
+    function hideEvents(events) {
+        if(events.length === 0) {
+            $('.logs .line.hidden').removeClass('hidden');
+            return;
+        }
+        var $e;
+        $('.logs .line').each(function(i, e) {
+            $e = $(e);
+
+            if(events.indexOf($e.attr('data-event-type')) !== -1) {
+                $e.addClass('hidden');
+            } else {
+                $e.removeClass('hidden');
+            }
+        });
+    }
+
+    var $toggleJoinPartInput = $controls.find('#toggle-join-part');
+    var localStorageKey = 'korobi-hidden-events';
+    var statusEvents = ['join', 'quit', 'part'];
+    var hiddenEvents = [];
+    var fromStorage;
+
+    if(window.localStorage && (fromStorage = localStorage.getItem(localStorageKey))) {
+        try {
+            hiddenEvents = JSON.parse(fromStorage);
+            var joinPartHidden = statusEvents.every(function(e) { return hiddenEvents.indexOf(e) !== -1; });
+            $toggleJoinPartInput.prop('checked', !joinPartHidden);
+            hideEvents(hiddenEvents);
+        } catch(ex) {
+            localStorage.removeItem(localStorageKey);
+        }
+    }
+
+    $toggleJoinPartInput.change(function() {
         var $this = $(this);
         if($this.prop('checked')) {
             hiddenEvents = hiddenEvents.filter(function(e) {
@@ -20,16 +51,8 @@ $(function() {
                     return arr.indexOf(e) === i;
                 });
         }
-
-        $('.logs .line').each(function(i, e) {
-            var $e = $(e);
-            var shouldHide = hiddenEvents.indexOf($e.attr('data-event-type')) !== -1;
-            if(shouldHide) {
-                $e.addClass('hidden');
-            } else {
-                $e.removeClass('hidden');
-            }
-        });
+        localStorage.setItem(localStorageKey, JSON.stringify(hiddenEvents));
+        hideEvents(hiddenEvents);
     });
 
     var $dateSelectorLabel = $controls.find('label[for=log-date]');
