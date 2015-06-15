fdd6240979c30eb1a5e74ff2ef2c54f8cfd3f587
bendem <online@bendem.be>
2015-06-15 19:53:12 +0200

    Refactor log highlighting js

--- src/Korobi/WebBundle/Resources/assets/js/application/irc/log/highlighting.js
+++ src/Korobi/WebBundle/Resources/assets/js/application/irc/log/highlighting.js
@@ -1,110 +1,114 @@
 $(function() {
+    var $logs = $('#logs');
+
     // Check if line highlighting is needed
-    if(!$('.logs:not(.tailing)').length) {
+    if($logs.is('.tailing')) {
         return;
     }
 
-    var activeLines = [],
-        containsLine = function(line) {
-            return activeLines.indexOf(line) != -1;
-        },
-        /**
-         * @param line
-         */
-        addLine = function(line) {
-            // we don't want zero
-            if(line === 0) {
+    var activeLines = [];
+    var lastSelection = null;
+
+    /**
+     * @returns {string}
+     */
+    function createHash() {
+        var result = 'L'; // hash (#) is automatically added
+        var first = true;
+        var lastAdded = null;
+        var rangeCreated = false;
+
+        activeLines.sort(function(e1, e2) { return e1 - e2; }).forEach(function(entry) {
+            if(first) {
+                first = false;
+                result += entry;
+                lastAdded = entry;
                 return;
             }
 
-            // only add if not already in array
-            if(activeLines.indexOf(line) == -1) {
-                activeLines.push(line);
-            }
-
-            // add class
-            $(".logs .line[data-line-num='" + line + "']").addClass('highlighted');
-        },
-        /**
-         * @param line
-         */
-        removeLine = function(line) {
-            // remove line from array
-            var index;
-            if((index = activeLines.indexOf(line)) > -1) {
-                activeLines.splice(index, 1);
-            }
-
-            // remove class
-            $(".logs .line[data-line-num='" + line + "']").removeClass('highlighted');
-        },
-        /**
-         * @returns {string}
-         */
-        createHash = function() {
-            var result = 'L'; // hash (#) is automatically added
-            var first = true;
-
-            activeLines.forEach(function(entry) {
-                if(!first) {
-                    result += ',';
+            if(entry === lastAdded + 1) {
+                if(!rangeCreated) {
+                    rangeCreated = true;
+                    result += '-';
                 }
+            } else {
+                if(rangeCreated) {
+                    result += lastAdded + ',' + entry;
+                    rangeCreated = false;
+                } else {
+                    result += ',' + entry;
+                }
+            }
 
-                first = false;
-                result += entry;
-            });
+            lastAdded = entry;
+        });
 
+        if(rangeCreated) {
+            result += lastAdded;
+        }
+
+        return result;
+    }
+
+    function highlightLines(lines) {
+        $logs.find('.highlighted').removeClass('highlighted');
+        lines.forEach(function(line) {
+            $logs.find('.line[data-line-num=' + line + ']').addClass('highlighted');
+        });
+    }
+
+    function parseHash() {
+        var result = [];
+        if($logs.length === 0) {
             return result;
-        },
-        hashChange = function() {
-            $('.logs .line').removeClass('highlighted');
+        }
 
-            var hash = window.location.hash;
+        var hash = window.location.hash;
+        var remainingPart = hash.substr(0, 2);
+        if(remainingPart != "#L") {
+            return result;
+        }
 
-            var remainingPart = hash.substr(0, 2);
-            if(remainingPart == "#L") {
-                if($('.logs').length !== 0) {
-                    var remainingParts = hash.substr(2).split(',');
-                    for(var i = 0; i < remainingParts.length; i++) {
-                        var part = remainingParts[i];
+        var remainingParts = hash.substr(2).split(',');
+        var number;
+        for(var i = 0; i < remainingParts.length; ++i) {
+            var part = remainingParts[i];
 
-                        if(part.indexOf('-') !== -1) {
-                            var re = /([0-9]+)-([0-9]+)/;
-                            var match = re.exec(part);
-                            if(match.length === 3) {
-                                var imin = Number(match[1]);
-                                var imax = Number(match[2]);
-                                if(imin > imax) {
-                                    var tempMax = imax;
-                                    imax = imin;
-                                    imin = tempMax;
-                                }
-                                for(var j = imin; j <= imax; j++) {
-                                    addLine(j);
-                                }
-                            }
-                        } else {
-                            var lineNum = Number(part);
-                            if(!isNaN(lineNum)) {
-                                addLine(lineNum);
-                            }
-                        }
-                    }
+            if(part.indexOf('-') === -1) {
+                number = Number(part);
+                if(!isNaN(number) && isFinite(number)) {
+                    result.push(number);
+                }
+            } else {
+                var match = part.split(/-/);
+                if(match.length !== 2) {
+                    continue;
+                }
+
+                var min = Number(match[0]);
+                var max = Number(match[1]);
+                if(min > max) {
+                    var temp = max;
+                    max = min;
+                    min = temp;
+                }
+
+                for(var j = min; j <= max; j++) {
+                    result.push(j);
                 }
             }
-        },
-        jumpToFirstLine = function() {
-            if(activeLines.length > 0) {
-                var shift = Math.min.apply(Math, activeLines);
-                var elem = $(".logs .line[data-line-num='" + shift + "']");
-                $(window).scrollTop(elem.offset().top - 0.33 * $(window).height());
-            }
-        };
+        }
 
-    // listen for hash changes
-    $(window).on('hashchange', function() {
-        hashChange();
-    });
+        return result;
+    }
+
+    function jumpToFirstLine() {
+        if(activeLines.length > 0) {
+            var shift = Math.min.apply(Math, activeLines);
+            var elem = $logs.find('.line[data-line-num=' + shift + ']');
+            $(window).scrollTop(elem.offset().top - 0.33 * $(window).height());
+        }
+    }
 
     // Allow lines to be highlighted by clicking the timestamp next to the log line.
     // The behaviour for clicks is as follows:
@@ -114,73 +118,70 @@ $(function() {
     // ---- add or remove (if line is already highlighted) a line
     // - Shift + Click:
     // ---- add/remove highlighting to a group of lines
-    $(document).on('mousedown', '.logs .timestamp', function(event) {
-        if(event.which == 3) {
+    $logs.find('.timestamp').mousedown(function(event) {
+        if(event.which === 3) {
             return;
         }
         event.preventDefault();
 
         // climb the dom to .line
         var line = $(this).closest('.line').data('line-num');
+        var index;
 
         // If ctrl/command is being held while clicking, either highlight the line or remove
         // existing highlighting from the line.
         if(event.ctrlKey || event.metaKey) {
-            // If the line is already highlighted, remove highlighting
-            if(activeLines.indexOf(line) != -1) {
-                removeLine(line);
+            if((index = activeLines.indexOf(line)) === -1) {
+                activeLines.push(line);
             } else {
-                addLine(line);
+                activeLines.splice(index, 1);
             }
+            lastSelection = line;
+
         } else if(event.shiftKey) {
-            // We'll assume the user wants to select a range here
-
-            // I'm arbitrarily deciding that a range will be made up of the last line they added to the list
-            // and the line they're clicking now.
-
-            var lastLine = activeLines[activeLines.length - 1];
-            if(activeLines.length <= 0) {
-                return;
-            } else if(containsLine(line)) {
-                return;
-            } else if(lastLine < line) {
-                // The just-selected line is in front of the last selected line. Great, we'll
-                // highlight all of the lines in between.
-
-                window.location.hash = window.location.hash + "," + lastLine + "-" + line;
-                hashChange();
-                return;
+            if(lastSelection === null) {
+                // The user never clicked before
+                activeLines = [line];
+                lastSelection = line;
             } else {
-                // The last line is after the one the user just selected. We'll start at the selected line
-                // and step to the next.
+                var first = Math.min(lastSelection, line);
+                var last =  Math.max(lastSelection, line);
+                console.log(first, last);
+                while(first <= last) {
+                    if((index = activeLines.indexOf(first)) === -1) {
+                        activeLines.push(first);
+                    } else {
+                        //activeLines.splice(index, 1);
+                    }
+                    ++first;
+                }
+                console.log(activeLines);
+            }
 
-                window.location.hash = window.location.hash + "," + line + "-" + lastLine;
-                hashChange();
+        } else {
+            if(activeLines.length === 1 && activeLines[0] === line) {
+                // The user just clicked the currently selected line
                 return;
             }
-        } else {
-            var alreadyExisted = (activeLines.indexOf(line) != -1 && activeLines.length === 1);
-            // ctrl is not being held, remove all and add new
-            activeLines.forEach(function(entry) {
-                removeLine(entry);
-            });
-            activeLines = [];
 
-            if(!alreadyExisted) {
-                addLine(line);
-            }
+            activeLines = [line];
+            lastSelection = line;
         }
+
+        highlightLines(activeLines);
 
         // set the new hash
         var hash = createHash();
-        if(hash !== 'L') {
-            window.location.hash = createHash();
-        } else {
+        if(hash === 'L') {
+            // No lines selected, remove the hash
             history.pushState('', document.title, window.location.pathname + window.location.search);
+        } else {
+            window.location.hash = createHash();
         }
     });
 
     // delay load
-    setTimeout(hashChange, 500);
-    setTimeout(jumpToFirstLine, 600);
+    activeLines = parseHash();
+    highlightLines(activeLines);
+    jumpToFirstLine();
 });
