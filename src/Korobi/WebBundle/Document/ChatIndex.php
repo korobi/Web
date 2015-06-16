7bb80fbf529a9b827eb57cb1b793b1474450d6d1
kashike <kashike@vq.lc>
2015-06-16 14:55:24 -0700

    Add chat index document + collection

---
+++ src/Korobi/WebBundle/Document/ChatIndex.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace Korobi\WebBundle\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+
+/**
+ * @MongoDB\Document(collection="chat_indexes",repositoryClass="Korobi\WebBundle\Repository\ChatIndexRepository")
+ */
+class ChatIndex {
+
+    /**
+     * @MongoDB\Id(strategy="auto")
+     */
+    protected $id;
+
+    /**
+     * @MongoDB\String
+     */
+    private $network;
+
+    /**
+     * @MongoDB\String
+     */
+    private $channel;
+
+    /**
+     * @MongoDB\Int
+     */
+    private $year;
+
+    /**
+     * @MongoDB\Int
+     */
+    private $day_of_year;
+
+    /**
+     * @MongoDB\Boolean
+     */
+    private $has_valid_content;
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * Get network
+     *
+     * @return string $network
+     */
+    public function getNetwork() {
+        return $this->network;
+    }
+
+    /**
+     * Set network
+     *
+     * @param string $network
+     * @return self
+     */
+    public function setNetwork($network) {
+        $this->network = $network;
+        return $this;
+    }
+
+    /**
+     * Get channel
+     *
+     * @return string $channel
+     */
+    public function getChannel() {
+        return $this->channel;
+    }
+
+    /**
+     * Set channel
+     *
+     * @param string $channel
+     * @return self
+     */
+    public function setChannel($channel) {
+        $this->channel = $channel;
+        return $this;
+    }
+
+    /**
+     * Get year
+     *
+     * @return int $year
+     */
+    public function getYear() {
+        return $this->year;
+    }
+
+    /**
+     * Set year
+     *
+     * @param int $year
+     * @return self
+     */
+    public function setYear($year) {
+        $this->year = $year;
+        return $this;
+    }
+
+    /**
+     * Get dayOfYear
+     *
+     * @return int $dayOfYear
+     */
+    public function getDayOfYear() {
+        return $this->day_of_year;
+    }
+
+    /**
+     * Set dayOfYear
+     *
+     * @param int $dayOfYear
+     * @return self
+     */
+    public function setDayOfYear($dayOfYear) {
+        $this->day_of_year = $dayOfYear;
+        return $this;
+    }
+
+    /**
+     * Get hasValidContent
+     *
+     * @return boolean $hasValidContent
+     */
+    public function getHasValidContent() {
+        return $this->has_valid_content;
+    }
+
+    /**
+     * Set hasValidContent
+     *
+     * @param boolean $hasValidContent
+     * @return self
+     */
+    public function setHasValidContent($hasValidContent) {
+        $this->has_valid_content = $hasValidContent;
+        return $this;
+    }
+}
