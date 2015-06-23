5f588b583d0f9db55b5e770e6f5c4143e42615a5
bendem <online@bendem.be>
2015-06-23 18:31:26 +0200

    Rewrite relative times again, using Kashike's idea this time

--- src/Korobi/WebBundle/Controller/HomeController.php
+++ src/Korobi/WebBundle/Controller/HomeController.php
@@ -14,37 +14,10 @@ class HomeController extends BaseController {
             ->getRecentlyActiveChannels(5)
             ->toArray();
 
-        $channels = [];
-        $now = time();
-        foreach($dbChannels as $channel) {
-            $channels[$channel->getChannel()] = $this->relativeTime($now, $channel->getLastValidContentAt()->getTimestamp());
-        }
-
-        return $this->render('KorobiWebBundle::home.html.twig', ["channels" => $channels]);
-    }
-
-    private function relativeTime($current, $previous) {
-        $msPerMinute = 60;
-        $msPerHour = $msPerMinute * 60;
-        $msPerDay = $msPerHour * 24;
-        $msPerMonth = $msPerDay * 30;
-        $msPerYear = $msPerDay * 365;
-
-        $elapsed = $current - $previous;
-
-        if ($elapsed < $msPerMinute) {
-            return round($elapsed) . ' seconds ago';
-        } else if ($elapsed < $msPerHour) {
-            return round($elapsed/$msPerMinute) . ' minutes ago';
-        } else if ($elapsed < $msPerDay ) {
-            return round($elapsed/$msPerHour ) . ' hours ago';
-        } else if ($elapsed < $msPerMonth) {
-            return round($elapsed/$msPerDay) . ' days ago';
-        } else if ($elapsed < $msPerYear) {
-            return round($elapsed/$msPerMonth) . ' months ago';
-        } else {
-            return round($elapsed/$msPerYear ) . ' years ago';
-        }
+        return $this->render('KorobiWebBundle::home.html.twig', [
+            'now' => time(),
+            'channels' => $dbChannels,
+        ]);
     }
 
 }
--- src/Korobi/WebBundle/Extension/ExtensionTwigFunctions.php
+++ src/Korobi/WebBundle/Extension/ExtensionTwigFunctions.php
@@ -15,6 +15,7 @@ class ExtensionTwigFunctions extends \Twig_Extension {
                 'is_safe' => ['html']
             ]),
             new \Twig_SimpleFunction('gmdate', [$this, 'gmdate']),
+            new \Twig_SimpleFunction('relativeTime', [$this, 'relativeTime']),
         ];
     }
 
@@ -42,5 +43,40 @@ class ExtensionTwigFunctions extends \Twig_Extension {
 
     public function gmdate($format, $timestamp = null) {
         return gmdate($format, $timestamp);
+    }
+
+    public function relativeTime($current, $previous) {
+        $secondsIn = [
+            'minute' => 60,
+            'hour'   => 60 * 60,
+            'day'    => 60 * 60 * 24,
+            'month'  => 60 * 60 * 24 * 30,
+            'year'   => 60 * 60 * 24 * 365,
+        ];
+
+        $elapsed = $current - $previous;
+
+        if($elapsed <= 0) {
+            return 'just now';
+        }
+
+        $prevSeconds = 60;
+        $prevName = 'seconds';
+        foreach($secondsIn as $name => $seconds) {
+            if($elapsed < $seconds) {
+                return self::plural(round($elapsed / $prevSeconds), $prevName) . ' ago';
+            }
+            $prevSeconds = $seconds;
+            $prevName = $name;
+        }
+        return 'Invalid time';
+    }
+
+    private static function plural($count, $word) {
+        $result = $count . ' ' . $word;
+        if($count > 1) {
+            return $result . 's';
+        }
+        return $result;
     }
 }
