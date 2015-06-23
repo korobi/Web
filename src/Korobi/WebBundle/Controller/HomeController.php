7ed51e212a3a156a29b334c3e4775d297746ee72
bendem <online@bendem.be>
2015-06-23 17:53:39 +0200

    Server side relative times

--- src/Korobi/WebBundle/Controller/HomeController.php
+++ src/Korobi/WebBundle/Controller/HomeController.php
@@ -14,6 +14,37 @@ class HomeController extends BaseController {
             ->getRecentlyActiveChannels(5)
             ->toArray();
 
-        return $this->render('KorobiWebBundle::home.html.twig', ["channels" => $dbChannels]);
+        $channels = [];
+        $now = time();
+        foreach($dbChannels as $channel) {
+            $channels[$channel->getChannel()] = $this->relativeTime($now, $channel->getLastValidContentAt()->getTimestamp());
+        }
+
+        return $this->render('KorobiWebBundle::home.html.twig', ["channels" => $channels]);
     }
+
+    private function relativeTime($current, $previous) {
+        $msPerMinute = 60;
+        $msPerHour = $msPerMinute * 60;
+        $msPerDay = $msPerHour * 24;
+        $msPerMonth = $msPerDay * 30;
+        $msPerYear = $msPerDay * 365;
+
+        $elapsed = $current - $previous;
+
+        if ($elapsed < $msPerMinute) {
+            return round($elapsed/1000) . ' seconds ago';
+        } else if ($elapsed < $msPerHour) {
+            return round($elapsed/$msPerMinute) . ' minutes ago';
+        } else if ($elapsed < $msPerDay ) {
+            return round($elapsed/$msPerHour ) . ' hours ago';
+        } else if ($elapsed < $msPerMonth) {
+            return round($elapsed/$msPerDay) . ' days ago';
+        } else if ($elapsed < $msPerYear) {
+            return round($elapsed/$msPerMonth) . ' months ago';
+        } else {
+            return round($elapsed/$msPerYear ) . ' years ago';
+        }
+    }
+
 }
