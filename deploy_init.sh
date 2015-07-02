754490aa6f9ef8ab21c3cf6fe2d47b416a938ee7
kashike <kashike@vq.lc>
2015-07-02 10:06:36 -0700

    Update deploy init path

--- deploy_init.sh
+++ deploy_init.sh
@@ -3,7 +3,7 @@
 echo "** Starting deploy"
 if [ "$(hostname)" == "chaos.ellune.net" ]; then
     echo "** Detected chaos.ellune.net, adding deploy key"
-    ssh-agent bash -c 'ssh-add /data/a/web/keys/korobi_web; /data/a/web/dev.korobi.io/deploy.sh'
+    ssh-agent bash -c 'ssh-add /data/a/web/keys/korobi_web; /data/a/capifony/dev.korobi.io/current/deploy.sh'
 else
     ./deploy.sh
 fi
