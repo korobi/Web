94d50b2ef92ab7778445c674a520a012f98b0e90
lol768 <lol768@lol768.com>
2015-07-12 17:41:14 +0100

    Expand unit test coverage

---
+++ src/Korobi/WebBundle/Test/Unit/AkioMessageBuilderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Korobi\WebBundle\Test\Unit;
+
+use Korobi\WebBundle\Deployment\TestOutputParser;
+use Korobi\WebBundle\Util\Akio;
+use Korobi\WebBundle\Util\AkioMessageBuilder;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * @package Korobi\WebBundle\Test\Unit
+ */
+class AkioMessageBuilderTest extends WebTestCase {
+
+    public function testSimpleMessage() {
+        $akioMock = $this->getMockBuilder('\Korobi\WebBundle\Util\Akio')->disableOriginalConstructor()->getMock();
+        $sut = new AkioMessageBuilder($akioMock);
+        $sut = $sut->hotPink()->text("Hello!");
+        $akioMock->expects($this->once())->method('sendMessage')->with($sut, "type");
+        $this->assertEquals($sut->getText(), "{C}13Hello!");
+        $sut->send("type");
+    }
+
+    public function testMoreComplexMessage() {
+        $akioMock = $this->getMockBuilder('\Korobi\WebBundle\Util\Akio')->disableOriginalConstructor()->getMock();
+        $sut = new AkioMessageBuilder($akioMock);
+        $sut = $sut->hotPink()->bold()->text("Hello ")->teal()->text("World!");
+        $akioMock->expects($this->once())->method('sendMessage')->with($sut, "type1");
+        $this->assertEquals($sut->getText(), "{C}13{B}Hello {C}10World!");
+        $sut->send("type1");
+    }
+
+}
