d7741bce51fe5139a6f3d090431ad77b4405192e
bendem <online@bendem.be>
2015-07-19 15:41:56 +0200

    Add commands to clear the cache and get cache stats

---
+++ src/Korobi/WebBundle/Console/LogCacheClearCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Korobi\WebBundle\Console;
+
+use Korobi\WebBundle\Util\FileCache;
+use Korobi\WebBundle\Util\FileUtil;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LogCacheClearCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this
+            ->setName('korobi:cache:logs:clear')
+            ->setDescription('Clears the logs cache')
+            ->addArgument('network', InputArgument::OPTIONAL)
+            ->addArgument('channel', InputArgument::OPTIONAL)
+            ;
+    }
+
+    protected function execute(InputInterface $in, OutputInterface $out) {
+        $network = $in->getArgument('network');
+        $channel = $in->getArgument('channel');
+
+        $cachePath = $this->getContainer()->getParameter('korobi.config')['log_cache_directory'];
+
+        if(!$network) {
+            FileUtil::removeRecursively($cachePath);
+            return;
+        }
+
+        $key = [$network];
+        if($channel) {
+            array_push($key, $channel);
+        }
+
+        $cache = new FileCache($cachePath);
+        dump($key);
+        dump($cache);
+
+        $count = $cache->remove($key);
+        $out->writeln('Cache cleared for "' . implode(DIRECTORY_SEPARATOR, $key) . '", ' . $count . ' files and folders');
+    }
+
+}
---
+++ src/Korobi/WebBundle/Console/LogCacheStatsCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Korobi\WebBundle\Console;
+
+use Korobi\WebBundle\Util\FileCache;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LogCacheStatsCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this
+            ->setName('korobi:cache:logs:stats')
+            ->setDescription('Get log cache statistics')
+        ;
+    }
+
+    protected function execute(InputInterface $in, OutputInterface $out) {
+        $cache = new FileCache($this->getContainer()->getParameter('korobi.config')['log_cache_directory']);
+        $stats = $cache->getStats();
+
+        $out->writeln('');
+        $out->writeln('Cache stats');
+        $out->writeln('|   hits     ' . $stats['hits']);
+        $out->writeln('|   misses   ' . $stats['misses']);
+        $out->writeln('|   changes  ' . $stats['changes']);
+        $out->writeln('');
+    }
+
+}
