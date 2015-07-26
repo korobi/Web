333609511db582a6ca6179961ae2849f38399592
lol768 <lol768@lol768.com>
2015-07-26 21:24:40 +0100

    That arg was there because it was used in the example ;)

--- src/Korobi/WebBundle/Handler/LogoutHandler.php
+++ src/Korobi/WebBundle/Handler/LogoutHandler.php
@@ -6,15 +6,8 @@ use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 class LogoutHandler implements LogoutSuccessHandlerInterface {
-
-    protected $router;
-
-    public function __construct(Router $router) {
-        $this->router = $router;
-    }
 
     public function onLogoutSuccess(Request $request) {
         // redirect the user to where they were before the login process begun.
