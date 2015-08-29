677e247d186a99f49de35ef3c308bf11331068d9
lol768 <lol768@lol768.com>
2015-08-29 19:38:02 +0100

    Well that worked well

--- src/Korobi/WebBundle/Exception/ChannelAccessException.php
+++ src/Korobi/WebBundle/Exception/ChannelAccessException.php
@@ -2,7 +2,7 @@
 
 namespace Korobi\WebBundle\Exception;
 
-class ChannelAccessException extends SecurityException {
+class ChannelAccessException extends SecurityException implements CustomPageExceptionInterface {
 
     private $network;
     private $channel;
@@ -13,21 +13,30 @@ class ChannelAccessException extends SecurityException {
      * @param string $channel
      */
     public function __construct($network, $channel) {
+        // misuse of exception code system?
+        parent::__construct("You do not have permission to view channel " . $channel . " on network " . $network . ".", 403);
         $this->network = $network;
         $this->channel = $channel;
     }
 
     /**
-     * @return string
+     * @return string The network to which the channel belonged.
      */
     public function getNetwork() {
         return $this->network;
     }
 
     /**
-     * @return string
+     * @return string The channel the user tried to view.
      */
     public function getChannel() {
         return $this->channel;
+    }
+
+    /**
+     * @return string Error page name used to display error details to user.
+     */
+    public function getViewName() {
+        return "channel-access";
     }
 }
--- src/Korobi/WebBundle/Listener/ExceptionListener.php
+++ src/Korobi/WebBundle/Listener/ExceptionListener.php
@@ -4,16 +4,56 @@
 namespace Korobi\WebBundle\Listener;
 
 
+use Korobi\WebBundle\Exception\CustomPageExceptionInterface;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
 class ExceptionListener {
+
+    /**
+     * @var TwigEngine
+     */
+    private $templatingEngine;
+
+    /**
+     * ExceptionListener constructor.
+     * @param TwigEngine $templatingEngine
+     */
+    public function __construct(TwigEngine $templatingEngine) {
+        $this->templatingEngine = $templatingEngine;
+    }
+
 
     /**
      * Handle kernel exceptions that come in and provide responses for exceptions we know about.
      * @param GetResponseForExceptionEvent $event
      */
     public function onKernelException(GetResponseForExceptionEvent $event) {
-        // TODO: Implementation
-        // $event->setResponse(...)
+        if ($event->getException() instanceof CustomPageExceptionInterface) {
+            /** @var CustomPageExceptionInterface|\Exception $exception */
+            $exception = $event->getException();
+            $response = $this->getResponseForException($exception);
+            $event->setResponse($response);
+            $event->stopPropagation();
+        }
+    }
+
+    /**
+     * @param $exception
+     * @return array
+     */
+    private function getViewParametersForException($exception) {
+        return ["exception" => $exception];
+    }
+
+    /**
+     * @param CustomPageExceptionInterface|\Exception $exception
+     * @return Response
+     */
+    private function getResponseForException($exception) {
+        $response = $this->templatingEngine->renderResponse($exception->getViewName(), $this->getViewParametersForException($exception));
+        $response->setStatusCode($exception->getCode());
+        return $response;
     }
 }
