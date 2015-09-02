b30136412481a92a3b9df36f29007df46fa93404
lol768 <lol768@users.noreply.github.com>
2015-09-02 20:26:53 +0100

    [Minor] Redirect sanatisation: How's this look?

--- src/Korobi/WebBundle/Handler/LogoutHandler.php
+++ src/Korobi/WebBundle/Handler/LogoutHandler.php
@@ -11,8 +11,10 @@ class LogoutHandler implements LogoutSuccessHandlerInterface {
     public function onLogoutSuccess(Request $request) {
         // redirect the user to where they were before the login process begun.
         $backUrl = $request->get("_destination");
-
-        $response = new RedirectResponse($backUrl);
+        if (substr($backUrl, 0, 1) === "/") {
+            $backUrl = substr($backUrl, 1);
+        }
+        $response = new RedirectResponse($request->getBaseUrl() . $backUrl);
         return $response;
     }
 
