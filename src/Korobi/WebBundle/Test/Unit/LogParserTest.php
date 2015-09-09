d990ec9cfe4914006f70953ef18246cb9a43c455
lol768 <lol768@lol768.com>
2015-09-09 22:21:35 +0100

    Fix complaints from CS fixer

--- src/Korobi/WebBundle/Test/Unit/LogParserTest.php
+++ src/Korobi/WebBundle/Test/Unit/LogParserTest.php
@@ -2,8 +2,6 @@
 
 namespace Korobi\WebBundle\Test\Unit;
 
-use Korobi\WebBundle\Document\Chat;
-use Korobi\WebBundle\IRC\Parser\IRCTextParser;
 use Korobi\WebBundle\IRC\Parser\LogParser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Translation\TranslatorInterface;
@@ -20,11 +18,11 @@ class LogParserTest extends WebTestCase {
 
 class DummyTranslator implements TranslatorInterface {
 
-    public function trans($id, array $parameters = array(), $domain = null, $locale = null) {
+    public function trans($id, array $parameters = [], $domain = null, $locale = null) {
         return "Cat.";
     }
 
-    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null) {
+    public function transChoice($id, $number, array $parameters = [], $domain = null, $locale = null) {
         return "Cat.";
     }
 
