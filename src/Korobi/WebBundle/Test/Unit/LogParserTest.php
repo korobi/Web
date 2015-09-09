8bc08847f11bedc5c460b0e7722d5a2d4206ea9f
lol768 <lol768@lol768.com>
2015-09-09 22:14:37 +0100

    Add LogParserTest class

---
+++ src/Korobi/WebBundle/Test/Unit/LogParserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Korobi\WebBundle\Test\Unit;
+
+use Korobi\WebBundle\Document\Chat;
+use Korobi\WebBundle\IRC\Parser\IRCTextParser;
+use Korobi\WebBundle\IRC\Parser\LogParser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class LogParserTest extends WebTestCase {
+
+    public function testTransformActorEscaping() {
+        $sut = new LogParser(new DummyTranslator());
+        $result = $sut->transformActor("<strong>HTML</strong><marquee>yay</marquee>");
+        $this->assertFalse(strpos($result, "<") !== false, "Left angle bracket must not be in result.");
+        $this->assertFalse(strpos($result, ">") !== false, "Right angle bracket must not be in result.");
+    }
+}
+
+class DummyTranslator implements TranslatorInterface {
+
+    public function trans($id, array $parameters = array(), $domain = null, $locale = null) {
+        return "Cat.";
+    }
+
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null) {
+        return "Cat.";
+    }
+
+    public function setLocale($locale) {
+        return;
+    }
+
+    public function getLocale() {
+        return "Cat.";
+    }
+}
