7fb7f0b96f6a26d863e95d3ec3a921992a9bab6e
lol768 <lol768@lol768.com>
2015-09-13 21:10:45 +0100

    Improve GitInfo performance, take 1

--- src/Korobi/WebBundle/Util/GitInfo.php
+++ src/Korobi/WebBundle/Util/GitInfo.php
@@ -13,44 +13,107 @@ class GitInfo {
 
     protected $branch;
     protected $hash;
+    protected $tempRef;
 
     /**
-     * @param $environment
-     * @param $rootDir
+     * @param string $environment Current environment's name.
+     * @param string $appDir Absolute path to app directory.
      */
-    public function __construct($environment, $rootDir) {
-        $this->updateData($rootDir, $environment);
+    public function __construct($environment, $appDir) {
+        $this->updateData(dirname($appDir));
     }
 
     /**
      * Force update data.
      *
-     * @param string $environment
+     * @param string $rootDir Absolute path to root directory.
      */
-    public function updateData($rootDir, $environment = 'dev') {
-        // Fast information retrieval for online environments
-        if (in_array($environment, self::ONLINE_ENVS)) {
-            if ($environment == 'prod') {
-                $this->branch = 'www1-stable';
+    public function updateData($rootDir) {
+        // This process is not simple! Please be careful when maintaining this code.
+        // Please refer to the flowchart in the PR for further information on how this works.
+
+        $process = [
+            "checkIsGitRepo",
+            "checkForGitHeadFile",
+            "checkForPackedRefs",
+            "useFallbackCommand"
+        ];
+        $gitDir = $rootDir . DIRECTORY_SEPARATOR . ".git" . DIRECTORY_SEPARATOR;
+        $firstItem = array_shift($process);
+        /** @var $firstItem Callable */
+        $this->$firstItem($gitDir, $process);
+    }
+
+    /*
+     * Begin chain-of-responsibility function definitions
+     * --------------------------------------------------
+     */
+
+    private function checkIsGitRepo($gitDir, $stack) {
+        if (file_exists($gitDir)) {
+            /** @var Callable $next */
+            $next = array_shift($stack);
+            $this->$next($gitDir, $stack);
+        } else {
+            $this->setBranchAndHash("unknown", "unknown");
+        }
+    }
+
+    private function checkForGitHeadFile($gitDir, $stack) {
+        if (file_exists($gitDir . "HEAD")) {
+            $headContents = fgets(fopen($gitDir . "HEAD", 'r')); // resource acquisition is initialization
+            $this->handleGitHeadFile($gitDir, $headContents, $stack);
+        } else {
+            $this->setBranchAndHash("unknown", "unknown");
+        }
+    }
+
+    private function handleGitHeadFile($gitDir, $headContents, $stack) {
+        if (strpos($headContents, "ref: ") === -1) {
+            $this->setBranchAndHash($headContents, $headContents);
+        } else {
+            $referencedFile = trim(substr($headContents, 5));
+            $this->branch = str_replace("/refs/heads/", "", $referencedFile);
+            $this->tempRef = $referencedFile;
+            if (!file_exists($gitDir . $referencedFile)) {
+                /** @var Callable $next */
+                $next = array_shift($stack);
+                $this->$next($gitDir, $stack);
             } else {
-                $ref = trim(file_get_contents($rootDir . '/../.git/HEAD'));
-                $this->branch = str_replace('refs/heads/', '', explode(' ', $ref)[1]);
+                $this->hash = file_get_contents($gitDir . $referencedFile);
             }
-            $this->hash = trim(file_get_contents($rootDir . '/../REVISION'));
-            return;
         }
+    }
 
-        // Accurate information retrieval for local environments
-        $branch = trim(`git rev-parse --abbrev-ref HEAD 2>&1`);
-        if (StringUtil::startsWith($branch, 'fatal: Not a git repository')) {
-            $this->branch = 'unknown';
-            $this->hash = 'unknown';
-            return;
+    private function checkForPackedRefs($gitDir, $stack) {
+        $refToLocate = $this->tempRef;
+        if (file_exists($gitDir . "packed-refs")) {
+            $packedRefData = file_get_contents($gitDir . "packed-refs");
+            $loc = strpos($packedRefData, $refToLocate);
+            $commitHash = "";
+            if ($loc !== false) {
+                while ($packedRefData[$loc] !== "\n" && $loc > 0) {
+                    $loc--;
+                    $commitHash .= $packedRefData[$loc];
+                }
+                $this->hash = strrev($commitHash);
+                return;
+            }
         }
+        /** @var Callable $next */
+        $next = array_shift($stack);
+        $this->$next($gitDir, $stack);
+    }
 
-        $this->branch = $branch;
+    private function useFallbackCommand($gitDir, $stack) {
+        $this->branch = trim(`git rev-parse --abbrev-ref HEAD 2>&1`);
         $this->hash = trim(`git rev-parse HEAD 2>&1`);
     }
+
+    /*
+     * --------------------------------------------------
+     * End chain-of-responsibility function definitions
+     */
 
     /**
      * @return string The name of the current git branch.
@@ -72,5 +135,10 @@ class GitInfo {
      */
     public function getShortHash($length = 7) {
         return substr($this->hash, 0, $length);
+    }
+
+    private function setBranchAndHash($branch, $hash) {
+        $this->branch = $branch;
+        $this->hash = $hash;
     }
 }
